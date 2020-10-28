5349c7511a9c3c80d15bf45ea69c5b51371c43bc
Oppong-Agyare ANOKYE NKANSAH <72044731+oanokyen@users.noreply.github.com>
2020-10-28 21:38:10 +0000

    Update ballot.py
    Added extra bracket to `int(input('Pick ball:  '))`

--- ballot.py
+++ ballot.py
@@ -5,25 +5,25 @@ if pera <= 4:
     print('Selection Sucessful')
     
 # Person B randomly picks another ball    
-perb = int(input('Pick ball:  ')
+perb = int(input('Pick ball:  '))
 while perb==pera:
     print('Error: Already chosen')
-    perb = int(input('Pick ball:  ')       
+    perb = int(input('Pick ball:  '))       
 else:
     print('Selection Sucessful')
                
  # Person C randomly picks another ball   
-perc = int(input('Pick ball:  ')
+perc = int(input('Pick ball:  '))
 while perc==pera or perc==perb:
     print('Error: Already chosen')
-    perc = int(input('Pick ball:  ')       
+    perc = int(input('Pick ball:  '))       
 else:
     print('Selection Sucessful')
 
 # Person D randomly picks last ball    
-perd = int(input('Pick ball:  ')
+perd = int(input('Pick ball:  '))
 while perd==pera or perd==perb or perd==perc:
     print('Error: Already chosen')
-    perd = int(input('Pick ball:  ')
+    perd = int(input('Pick ball:  '))
 else:
     print('Selection Sucessful')
